6ae6463f1e8d264730e2990e92b9d3d466fb98a0
faustofusse <faustofusse@gmail.com>
2020-08-17 00:55:40 -0300

    Database url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
-module.exports.DATABASE_URI = 'mongodb://127.0.0.1:27017/discoverme'
+module.exports.DATABASE_URI = 'mongodb+srv://faustofusse:1234@dme-cluster.bder9.mongodb.net/discoverme?retryWrites=true&w=majority'
+// module.exports.DATABASE_URI = 'mongodb://127.0.0.1:27017/discoverme'
 // export const DATABASE_URI = 'mongodb://restoarDb:lagarto24@ds117816.mlab.com:17816/restoar'
 module.exports.JWT_SECRET = 'SecretPassword';
